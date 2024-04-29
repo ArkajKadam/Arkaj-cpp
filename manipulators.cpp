ceaf7a18b1b12ed35250c7f58b63af65dac15006
ArkajKadam <168397133+ArkajKadam@users.noreply.github.com>
2024-04-28 23:53:09 -0500

    Add files via upload

---
+++ manipulators.cpp
@@ -0,0 +1,19 @@
+#include<iomanip>
+#include<iostream>
+using namespace std;
+int main()
+{
+	double A = 100;
+	double B = 2001.5251;
+	double C = 201455.2646;
+	
+	cout<<hex<<left<<showbase<<nouppercase;
+	cout<<(long long)A<<endl;
+	cout<<setbase(10)<<right<<setw(15)
+	    <<setfill('_')<<showpos
+	    <<fixed<<setprecision(2);
+	cout<<B<<endl;
+	cout<<scientific<<uppercase
+	    <<noshowpos<<setprecision(9);
+	cout<<C<<endl;
+}
